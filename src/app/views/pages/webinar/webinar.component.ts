e21478b180834ed7dd5a35e27df375df254f1960
ChandanPolai <chandanpolai26@gmail.com>
2025-09-07 19:01:15 +0530

    webinar file

---
+++ src/app/views/pages/webinar/webinar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-webinar',
+  standalone: true,
+  imports: [],
+  templateUrl: './webinar.component.html',
+  styleUrl: './webinar.component.scss'
+})
+export class WebinarComponent {
+
+}
